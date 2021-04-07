7b31f6d44ca595e650b2afb31d4beafcd20603e7
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2021-04-07 20:04:03 +0300

    test main axios-like logic, start data-table component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,42 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/peiko-logo-mini.png">
-    <h1>Test</h1>
-    <hr>
+    <DataTable />
   </div>
 </template>
 
 <script>
+import DataTable from '@/components/DataTable/DataTable';
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: { DataTable },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+
+html {
+  font-family: Avenir, -apple-system, BlinkMacSystemFont,
+  'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+#app {
+  min-height: 100vh;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/DataTable/DataTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="data-table">
+    <button @click.prevent="onClick" class="data-table__action">Get Data</button>
+    <div v-if="!data" class="data-table__error">No Data</div>
+    <div v-else class="data-table__report">
+      {{ data }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { payload } from '@/mocData';
+import simulateAsyncReq from '@/plugins/getDataFunc';
+
+export default {
+  data() {
+    return {
+      data: null,
+    }
+  },
+  methods: {
+    onClick() {
+      simulateAsyncReq(payload).then((response) => {
+        this.data = response;
+      })
+      .catch(() => {
+        this.data = false;
+      });
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import "DataTable.scss";
+</style>
